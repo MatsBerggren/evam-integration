919caaeb521c4fab08a258dd345ad08e09c55049
Mats Berggren <mats.berggren@outlook.com>
2023-11-18 13:32:09 +0100

    Add CrossOrigin annotation to EvamController

--- src/main/java/com/openapi3demo/bookapiservice/controller/EvamController.java
+++ src/main/java/com/openapi3demo/bookapiservice/controller/EvamController.java
@@ -1,6 +1,7 @@
 package com.openapi3demo.bookapiservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import com.openapi3demo.bookapiservice.service.EvamService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 @Tag(name = "Evam API", description = "API collection for CRUD operations on Book Resource")
 public class EvamController {
